be686e5e8adf94f3280c89a3fc37a70ac852933f
Erind Hoxha <erind_cbh@hotmail.com>
2018-10-07 16:18:31 +1300

    Some changes

--- js/data.js
+++ js/data.js
@@ -7,5 +7,56 @@ var levelData = {
         "icon": "earthspirit-01.png",
         "image": "earthspirit-01.png",
         "answer": "earth spirit"
-    }]
-}
+    },
+    {
+        "icon": "alchemist-01.png",
+        "image": "alchemist-01.png",
+        "answer": "alchemist"
+    },
+    {
+        "icon": "axe-01.png",
+        "image": "axe-01.png",
+        "answer": "axe"
+    },
+    {
+        "icon": "beastmaster-01.png",
+        "image": "beastmaster-01.png",
+        "answer": "beastmaster"
+    },
+    {
+        "icon": "bloodseeker-01.png",
+        "image": "bloodseeker-01.png",
+        "answer": "bloodseeker"
+    },
+    {
+        "icon": "bountyhunter-01.png",
+        "image": "bountyhunter-01.png",
+        "answer": "bounty hunter"
+    },
+    {
+        "icon": "brewmaster-01.png",
+        "image": "brewmaster-01.png",
+        "answer": "brewmaster"
+    },
+    {
+        "icon": "bristleback-01.png",
+        "image": "bristleback-01.png",
+        "answer": "bristleback"
+    },
+    {
+        "icon": "broodmother-01.png",
+        "image": "broodmother-01.png",
+        "answer": "broodmother"
+    },
+    {
+        "icon": "centaur-01.png",
+        "image": "centaur-01.png",
+        "answer": "centaur"
+    },
+    {
+        "icon": "chaosknight-01.png",
+        "image": "chaosknight-01.png",
+        "answer": "chaos knight"
+    }
+
+]}
--- js/main.js
+++ js/main.js
@@ -1,63 +1,3 @@
-
-
-// MODAL
-// Get the modal
-var modal = document.getElementById('myModal');
-// Get the button that opens the modal
-var btn = document.getElementById("stage-icon");
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-// When the user clicks on the button, open the modal 
-// btn.onclick = function() {
-//     modal.style.display = "block";
-// }
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-
-
-function loadStage(stageNumber) {
-
-    var stage = levelData["level1"][stageNumber];
-
-    currentAnswer = stage.answer;
-
-    $(".modal-img").attr('src', "img/" + stage["image"]);
-    // Finally, show the stage
-    
-    modal.style.display = "block";
-}
-String.prototype.levenstein = function (string) {
-    var a = this,
-        b = string + "",
-        m = [],
-        i, j, min = Math.min;
-
-    if (!(a && b)) return (b || a).length;
-
-    for (i = 0; i <= b.length; m[i] = [i++]);
-    for (j = 0; j <= a.length; m[0][j] = j++);
-
-    for (i = 1; i <= b.length; i++) {
-        for (j = 1; j <= a.length; j++) {
-            m[i][j] = b.charAt(i - 1) == a.charAt(j - 1) ?
-                m[i - 1][j - 1] :
-                m[i][j] = min(
-                    m[i - 1][j - 1] + 1,
-                    min(m[i][j - 1] + 1, m[i - 1][j]))
-        }
-    }
-
-    return m[b.length][a.length];
-}
 // NAV BAR
 $(document).ready(function(){
     
@@ -87,6 +27,60 @@ $(document).ready(function(){
         // PER ANSWER
     })
 });
+
+
+// MODAL
+// Get the modal
+var modal = document.getElementById('myModal');
+// Get the button that opens the modal
+var btn = document.getElementById("stage-icon");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+// When the user clicks on the button, open the modal 
+// btn.onclick = function() {
+//     modal.style.display = "block";
+// }
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+function loadStage(stageNumber) {
+    var stage = levelData["level1"][stageNumber];
+    currentAnswer = stage.answer;
+    $(".modal-img").attr('src', "img/" + stage["image"]);
+    // Finally, show the stage
+    modal.style.display = "block";
+}
+String.prototype.levenstein = function (string) {
+    var a = this,
+        b = string + "",
+        m = [],
+        i, j, min = Math.min;
+
+    if (!(a && b)) return (b || a).length;
+
+    for (i = 0; i <= b.length; m[i] = [i++]);
+    for (j = 0; j <= a.length; m[0][j] = j++);
+
+    for (i = 1; i <= b.length; i++) {
+        for (j = 1; j <= a.length; j++) {
+            m[i][j] = b.charAt(i - 1) == a.charAt(j - 1) ?
+                m[i - 1][j - 1] :
+                m[i][j] = min(
+                    m[i - 1][j - 1] + 1,
+                    min(m[i][j - 1] + 1, m[i - 1][j]))
+        }
+    }
+
+    return m[b.length][a.length];
+}
 
 // MODAL INDEX.HTML SHOW/HIDE
 $(window).on('load',function(){
